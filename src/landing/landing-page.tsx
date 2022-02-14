76383aecda570e63b889cca4a9635421016c858e
Pavel Pleshko <pavel.pleshko@sbgenomics.com>
2022-02-14 17:11:15 +0100

    feat(landing): add illustrations

--- src/landing/landing-page.tsx
+++ src/landing/landing-page.tsx
@@ -1,30 +1,74 @@
 import { Button, Grid } from '@mui/material';
 import * as React from 'react';
+import MainNavigation from '../shared/main-navigation/main-navigation';
 import Jumbotron from '../shared/ui/jumbotron/jumbotron';
 import './landing-page.scss';
+import ValuesCards from './values-cards/values-cards';
+import dashboardImage from '../static/images/dashboard.svg';
 
 function LandingPage () {
 
     return (
         <div className="LandingPage">
-            <div className="LandingPage__hero">
-                <Jumbotron>
-                    <div className="LandingPage__hero-container">
-                        <div className="LandingPage__hero-bg"/>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12} md={6} className="LandingPage__hero-content">
-                                <h1>Lending and borrowing at a favorable rate</h1>
-                                <p>
-                                    Discover the new way of investing
-                                </p>
-                                <Button color="secondary" variant="contained">
-                                    Browse assets
-                                </Button>
+            <div className="LandingPage__hero-wrapper">
+                <MainNavigation/>
+                <div className="LandingPage__hero">
+                    <div className="LandingPage__hero-bg"/>
+                    <Jumbotron>
+                        <div className="LandingPage__hero-container">
+                            <Grid container spacing={ 4 }>
+                                <Grid item md={ 1 } xs={ 0 }/>
+                                <Grid item xs={ 12 } md={ 6 } className="LandingPage__hero-content">
+                                    <h1>Lend and borrow at a favorable rate</h1>
+                                    <p>
+                                        Discover the new way of investing
+                                    </p>
+                                    <Button variant="contained">
+                                        Browse assets
+                                    </Button>
+                                </Grid>
                             </Grid>
-                        </Grid>
-                    </div>
-                </Jumbotron>
+                        </div>
+                    </Jumbotron>
+                </div>
             </div>
+
+            <section className="LandingPage__content-section LandingPage__content-section__dark">
+                <h2 className="LandingPage__content-section-header">Our values</h2>
+                <ValuesCards/>
+            </section>
+
+
+            <section className="LandingPage__content-section LandingPage__content-section__dark">
+                <h2 className="LandingPage__content-section-header">Track your income</h2>
+                <Grid container>
+                    <Grid item xs={ 6 }>
+                        <p>
+                            You can monitor the real-time earnings on the user dashboard. This will
+                            help you to invest and borrow intelligently.
+                        </p>
+                        <Button variant="contained">
+                            Connect now
+                        </Button>
+                    </Grid>
+
+                    <Grid item xs={ 1 }/>
+                    <Grid item xs={ 4 }>
+                        <img width="100%" src={ dashboardImage } alt="Dashboard illustration"/>
+                    </Grid>
+                </Grid>
+            </section>
+
+            <section className="LandingPage__content-section" style={ { textAlign: 'center' } }>
+
+                <h1>More than <b>12</b> asset pools with more than <b>3.000.000$</b> worth assets</h1>
+
+                <Button variant="contained">
+                    See all
+                </Button>
+
+
+            </section>
         </div>
     );
 }
---
+++ src/landing/values-cards/values-cards.tsx
@@ -0,0 +1,52 @@
+import { Card, CardContent, CardHeader, Grid, Typography } from '@mui/material';
+import React from 'react';
+import styles from './value-cards.module.scss';
+
+interface ValueCard {
+    title: string;
+    description: string;
+}
+
+const cardsData: ValueCard[] = [
+    {
+        title: 'Security',
+        description: `Our system is built with security in mind and have been audited by the professionals.`
+    },
+    {
+        title: 'Simplicity',
+        description: `We guide users through every operation available in the most simple way possible.`,
+    },
+    {
+        title: 'Transparency',
+        description: `We provide most up-to-date information on the market with nice visualisations to help
+        with finding the most relevant numbers.`,
+    }
+];
+
+function ValuesCards () {
+
+    return <Grid container spacing={ 3 }>
+        { cardsData.map((card, index) => {
+            return (
+                <Grid item md={ 4 } xs={ 12 } key={ index }>
+                    <Card className={styles.ValueCard}>
+
+                        <CardHeader className={styles.ValueCard__header} title={ card.title }/>
+
+                        <CardContent>
+                            <span className={styles.ValueCard__number}>
+                                0{index + 1}
+                            </span>
+                            <Typography variant="body2">
+                                { card.description }
+                            </Typography>
+                        </CardContent>
+
+                    </Card>
+                </Grid>
+            );
+        }) }
+    </Grid>;
+}
+
+export default ValuesCards;
